0953ff08bb344c2e3d3e8e2dff460fe0578a52ed
Akash Padwal <akashpadwal21@gmail.com>
2023-08-12 17:58:22 +0530

    -feat * creation * deletion * searching

--- Tree/BST/bst.ts
+++ Tree/BST/bst.ts
@@ -1,14 +1,31 @@
-const bst: number[] = [];
+const bst: number[] | any[] = [];
 
-const unsortedElements = [5, 2, 3, 7, 8, 1, 6];
+// Arrays is not the optimal way to represent binary search tree
+// Hence, some errors are there when deletion operation occures since the children are not respresented properly
+const unsortedElements = [5, 2, 3, 7, 8, 1, 6, 12, 15];
+// const unsortedElements = [5, 2, 3, 7, 8, 1, 6];
 
 //          5
 //    2           7
 // 1     3     6     8
+//                      12
+//                          15
 
-// 5 -> 2, 7
-// 2 -> 1, 3
-// 7 -> 6, 8
+// 5  -> 2, 7
+// 2  -> 1, 3
+// 7  -> 6, 8
+// 8  -> null, 12
+// 12 -> null, 15
+
+//indexes
+
+// 0 -> 1,2
+// 1 -> 3,4
+// 2 -> 5,6
+// 3 -> 7,8
+// 4 -> 9,10
+// 5 -> 11,12
+// 6 -> 13,14
 
 function buildBst(element: number) {
   let root = 0;
@@ -47,16 +64,89 @@ function findElement(element: number, position: number) {
     let leftChildNode = position * 2 + 1;
     if (!bst[leftChildNode]) {
       console.log("Element not present!");
-      return;
+      return null;
     }
-    findElement(element, leftChildNode);
+    return findElement(element, leftChildNode);
   } else {
     let rightChildNode = position * 2 + 2;
     if (!bst[rightChildNode]) {
       console.log("Element not present!");
-      return;
+      return null;
     }
-    findElement(element, rightChildNode);
+    return findElement(element, rightChildNode);
+  }
+}
+
+function deleteElement(element: number, position: number) {
+  console.log("Deleting element >>>", element, "At position >>>", position);
+  //child of root node found to delete
+  const leftChildNode = position * 2 + 1;
+  const rightChildNode = position * 2 + 2;
+
+  //traversing logic
+  //get the element with lowest value from right sub tree by traversing to left side
+  const getInOrderSuccessor = (position: number): any => {
+    // console.log("InOrder Position >>>", position, "Element >>>", bst[position]);
+    let rightChild = position * 2 + 2;
+    let leftChild = position * 2 + 1;
+
+    //if the node is leaf node then delete the position without swap
+    if (!bst[rightChild] && !bst[leftChild]) {
+      // console.log("Returning since lef node...");
+      return position;
+    }
+
+    //if there is only single child then directly swap
+    if (!bst[rightChild] || !bst[leftChild]) {
+      // console.log(
+      //   "Returning since single child...",
+      //   bst[rightChild],
+      //   bst[leftChild]
+      // );
+      if (bst[rightChild]) {
+        return rightChild;
+      } else {
+        return leftChild;
+      }
+    }
+
+    if (rightChildNode === rightChild) {
+      // console.log("Right child traverse...");
+      //first traverse right node and then keep left
+      return getInOrderSuccessor(rightChild);
+    } else {
+      // console.log("Left child traverse...");
+      if (bst[leftChild]) {
+        return getInOrderSuccessor(leftChild);
+      } else {
+        // console.log("Returning position", position);
+        return position;
+      }
+    }
+  };
+
+  //execution
+  const swapPosition = getInOrderSuccessor(position);
+  // console.log("Swap position", swapPosition);
+  if (swapPosition && bst[swapPosition]) {
+    // console.log("Swap element", bst[swapPosition]);
+    if (swapPosition === position) {
+      bst.splice(swapPosition, 1);
+    } else {
+      let swap = bst[swapPosition];
+      bst.splice(swapPosition, 1); // bst[swapPosition] = null;
+      bst[position] = swap;
+    }
+  }
+}
+
+function removeElement(element: number, position: number) {
+  // console.log("Removing element >>>", element);
+  //finding the element and getting the position if present
+  const elementPosition = findElement(element, position);
+  // console.log("Element position >>>", elementPosition);
+  if (typeof elementPosition === "number") {
+    deleteElement(element, elementPosition);
   }
 }
 
@@ -65,7 +155,9 @@ function start() {
     buildBst(element);
   });
   console.log("Bst", bst);
-  findElement(4, 0);
+  // findElement(4, 0);
+  removeElement(8, 0);
+  console.log("Bst after deletion", bst);
 }
 
 start();
