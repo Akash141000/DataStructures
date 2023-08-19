2402f72892f604cd463336bd53586bdb7164d23e
Akash Padwal <akashpadwal21@gmail.com>
2023-08-19 15:17:35 +0530

    -feat * Prims algo working

--- Graph/Spanning Tree/primsAlgo.ts
+++ Graph/Spanning Tree/primsAlgo.ts
@@ -1,8 +1,9 @@
 import {
   NodeTreeElement,
   buildTreeHeap,
+  decreaseKey,
   removeElementMinHeap,
-} from "../../Heap/heapTree";
+} from "../../Heap/heapWithDecreaseKey";
 
 //
 //         A
@@ -16,125 +17,114 @@ import {
 //              10
 //
 
+// const graph: { [key: string]: string[] } = {
+//   A: ["B"],
+//   B: ["C", "D", "E", "A"],
+//   C: ["B", "E", "D"],
+//   D: ["B", "E", "C"],
+//   E: ["C", "D", "B"],
+// };
+
+// const edgeWeights: { [key: string]: number } = {
+//   AB: 70,
+//   BD: 60,
+//   BC: 20,
+//   BE: 40,
+//   DC: 50,
+//   CE: 30,
+//   DE: 10,
+// };
+
+//// Graph 2
+
+//                A
+//             /  |  \
+//          7/    |2   \6
+//         /   6  |   6  \
+//      B ------- C ------- D
+//        \     /   \     /
+//        4\  8/    5\   /3
+//          \ /       \ /
+//           E  -----  F
+//                7
+
 const graph: { [key: string]: string[] } = {
-  A: ["B"],
-  B: ["C", "D", "E", "A"],
-  C: ["B", "E", "D"],
-  D: ["B", "E", "C"],
-  E: ["C", "D", "B"],
+  A: ["B", "C", "D"],
+  B: ["A", "C", "E"],
+  C: ["A", "B", "D", "F", "E"],
+  D: ["A", "C", "F"],
+  E: ["B", "C", "F"],
+  F: ["C", "D", "E"],
 };
 
 const edgeWeights: { [key: string]: number } = {
-  AB: 70,
-  BD: 60,
-  BC: 20,
-  BE: 40,
-  DC: 50,
-  CE: 30,
-  DE: 10,
+  AB: 7,
+  AC: 2,
+  AD: 6,
+  CB: 6,
+  CD: 6,
+  BE: 4,
+  CE: 8,
+  CF: 5,
+  DF: 3,
+  EF: 7,
 };
 
-const graphAfter: { [key: string]: string[] } = {
-  A: [],
-  B: [],
-  C: [],
-  D: [],
-  E: [],
-};
+function start() {
+  //keep track of visited nodes
+  const visitedNodes: { [key: string]: string } = {};
 
-let visitedNodesDFT: { [key: string]: boolean } = {}; // let is used since dft used for cycle detection
+  //first initialize all node to infinity in minHeap
+  const valueAsInfinity = 100000;
+  const graphNodes: NodeTreeElement[] = [];
+  const nodeSequence: string[] = [];
+  Object.keys(graph).forEach((node) => {
+    const treeNode = new NodeTreeElement(node, valueAsInfinity, "");
+    graphNodes.push(treeNode);
+  });
 
-function dft(
-  node: string,
-  graph: { [key: string]: string[] },
-  cyclePresent?: boolean[]
-) {
-  console.log("Node -->", node);
-  visitedNodesDFT[node] = true; //mark node as visited
+  //build Heap for  the infinity Nodes
+  const minHeap = buildTreeHeap(graphNodes);
+  console.log("MinHeap Infinity --->", minHeap);
 
-  const adjacentNodes = graph[node];
-  if (adjacentNodes.length > 0) {
+  //remove one element to start
+  let minValueNode = new NodeTreeElement("", 0, "");
+
+  //do while all graph nodes are visited
+  while (Object.keys(visitedNodes).length !== Object.keys(graph).length) {
+    minValueNode = removeElementMinHeap();
+
+    //register node as visited
+    if (minValueNode.data) {
+      visitedNodes[minValueNode.data!] = minValueNode.data;
+    }
+
+    // add node to node traverse sequence
+    if (minValueNode.link) {
+      nodeSequence.push(`${minValueNode.link}${minValueNode.data!}`);
+    }
+    console.log("Min Value Node --->", minValueNode);
+
+    //find the adjacent nodes
+    const adjacentNodes = graph[minValueNode.data!];
+    console.log("Adjacent Node --->", adjacentNodes);
+
+    //looping through Adjacent Nodes
     adjacentNodes.forEach((adjacentNode) => {
-      if (visitedNodesDFT[adjacentNode] && cyclePresent) {
-        cyclePresent.push(true);
-      } else if (!visitedNodesDFT[adjacentNode]) {
-        dft(adjacentNode, graph, cyclePresent);
+      //find the edge value
+      let edgeValue =
+        edgeWeights[`${minValueNode.data!}${adjacentNode}`] ||
+        edgeWeights[`${adjacentNode}${minValueNode.data!}`];
+      console.log("Edge Value", edgeValue);
+
+      //if the node is not visited yet then decrease key value
+      if (!visitedNodes[adjacentNode]) {
+        decreaseKey(adjacentNode, edgeValue, minValueNode.data!);
       }
     });
   }
-}
 
-function start() {
-  const visitedNodes = {};
-  let nodeEdges: any[] = [];
-  let validEdges: any[] = [];
-  let currentNode: string | null = null;
-  // Object.keys(graph).forEach((node, index) => {
-  let node = "B";
-  currentNode = node;
-  nodeEdges = [];
-  graph[node].forEach((adjacentNode, count) => {
-    const edgeWeight =
-      edgeWeights[`${node + adjacentNode}`] ||
-      edgeWeights[`${adjacentNode + node}`];
-    nodeEdges.push(new NodeTreeElement(`${node + adjacentNode}`, edgeWeight));
-  });
-  console.log("NODE EDGES", nodeEdges);
-  const minHeap = buildTreeHeap(nodeEdges);
-  console.log("MIN HEAP", minHeap);
-  const minNode = removeElementMinHeap();
-  console.log("MIN NODE", minNode, "Data", minNode.value);
-  const nextMinNode = minNode.data?.split("")[1];
-  // minHeap.forEach((node) => {
-  //   console.log("Edge ----------->", minNode.data);
-  //   const nodes = minNode.data!.split("");
-  //   if (nodes?.length >= 2) {
-  //     graphAfter[nodes[0]].push(nodes[1]);
-  //   }
-  //   visitedNodesDFT = {}; //visited nodes reset
-  //   let cyclePresent: boolean[] = [];
-  //   dft(nodes[0], graphAfter, cyclePresent);
-
-  //   if (cyclePresent.some((val) => val === true)) {
-  //     const indexToRemove = graphAfter[nodes[0]].findIndex(
-  //       (element) => element === nodes[1]
-  //     );
-  //     graphAfter[nodes[0]].splice(indexToRemove);
-  //   } else {
-  //     validEdges.push(minNode.data);
-  //   }
-  // });
-  // });
-
-  // const minHeap = buildTreeHeap(nodeEdges);
-  // let validEdges: any[] = [];
-  // minHeap.forEach((node) => {
-  //   const minNode = removeElementMinHeap();
-  //   console.log("Edge ----------->", minNode.data);
-  //   const nodes = minNode.data!.split("");
-  //   if (nodes?.length >= 2) {
-  //     graphAfter[nodes[0]].push(nodes[1]);
-  //   }
-  //   visitedNodesDFT = {}; //visited nodes reset
-  //   let cyclePresent: boolean[] = [];
-  //   dft(nodes[0], graphAfter, cyclePresent);
-
-  //   if (cyclePresent.some((val) => val === true)) {
-  //     const indexToRemove = graphAfter[nodes[0]].findIndex(
-  //       (element) => element === nodes[1]
-  //     );
-  //     graphAfter[nodes[0]].splice(indexToRemove);
-  //   } else {
-  //     validEdges.push(minNode.data);
-  //   }
-  // });
-  // validEdges.forEach((edge) => {
-  //   const nodes = edge.split("");
-  //   graphAfter[nodes[1]].push(nodes[0]);
-  // });
-  // console.log("Acyclic graph -->", graphAfter);
-  // visitedNodesDFT = {};
-  // dft("A", graphAfter);
+  console.log("Node traversal sequence --->", nodeSequence);
 }
 
 start();
