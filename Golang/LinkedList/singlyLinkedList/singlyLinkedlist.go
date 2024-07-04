9aee03757db8992879405215fcdd8d3458ffaa73
Akash Padwal <akashpadwal21@gmail.com>
2024-07-04 21:52:19 +0530

    -feat * singly linkedlist node deletion

--- Golang/LinkedList/singlyLinkedList/singlyLinkedlist.go
+++ Golang/LinkedList/singlyLinkedList/singlyLinkedlist.go
@@ -22,6 +22,28 @@ func AddNewNode(data string) *utils.ListNode {
 	return listNode
 }
 
+func DeleteNode(position int) *utils.ListNode {
+	var nodeToDel **utils.ListNode = &HeadNode
+	var prevNode *utils.ListNode
+	var deletedNode utils.ListNode
+
+	if position == 1 {
+		deletedNode = *HeadNode
+		HeadNode = (*nodeToDel).Next
+		return &deletedNode
+	}
+
+	for i := 1; i < position && (*nodeToDel).Next != nil; i++ {
+		prevNode = *nodeToDel
+		nodeToDel = &(*nodeToDel).Next
+	}
+
+	deletedNode = *prevNode.Next
+	prevNode.Next = (*nodeToDel).Next
+
+	return &deletedNode
+}
+
 func GetLastNode() *utils.ListNode {
 	LastNode := &HeadNode
 	for (*LastNode).Next != nil {
@@ -31,6 +53,7 @@ func GetLastNode() *utils.ListNode {
 }
 
 func PrintLinkedList() {
+	fmt.Println("Printing linked list....")
 	for listNode := HeadNode; listNode != nil; {
 		fmt.Println(listNode.Data, "-->")
 		listNode = listNode.Next
@@ -43,6 +66,14 @@ func main() {
 	AddNewNode("2")
 	AddNewNode("3")
 	AddNewNode("4")
+
+	PrintLinkedList()
+
+	deletedNode := DeleteNode(2)
+	fmt.Println("Deleted Node", deletedNode)
+
+	nextDeletedNode := DeleteNode(1)
+	fmt.Println("Deleted Node", nextDeletedNode)
 
 	PrintLinkedList()
 }
