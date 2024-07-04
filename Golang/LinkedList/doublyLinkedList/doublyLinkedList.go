cc55c345444e05f2fc13f612171378648f9feb6f
Akash Padwal <akashpadwal21@gmail.com>
2024-07-04 23:43:08 +0530

    -feat * doubly linked list node deletion

--- Golang/LinkedList/doublyLinkedList/doublyLinkedList.go
+++ Golang/LinkedList/doublyLinkedList/doublyLinkedList.go
@@ -31,7 +31,41 @@ func GetLastNode() *utils.ListNode {
 	return *LastNode
 }
 
+func DeleteNode(position int) *utils.ListNode {
+	var nodeToDel **utils.ListNode = &HeadNode
+	var deletedNode utils.ListNode
+
+	if position == 1 {
+		deletedNode = *HeadNode
+		HeadNode = (*nodeToDel).Next
+		(*nodeToDel).Prev = nil
+		return &deletedNode
+	}
+
+	for i := 1; i < position && (*nodeToDel).Next != nil; i++ {
+		nodeToDel = &(*nodeToDel).Next
+	}
+
+	deletedNode = **nodeToDel
+
+	if (*nodeToDel).Next != nil {
+		(*nodeToDel).Next.Prev = (*nodeToDel).Prev
+	} else {
+		(*nodeToDel).Prev.Next = nil
+		return &deletedNode
+	}
+
+	if (*nodeToDel).Prev != nil {
+		(*nodeToDel).Prev.Next = (*nodeToDel).Next
+	}
+
+	deletedNode = **nodeToDel
+
+	return &deletedNode
+}
+
 func PrintLinkedList() {
+	fmt.Println("Printing linked list....")
 	for listNode := HeadNode; listNode != nil; {
 		fmt.Println("Prev", listNode.Prev, "<-----", listNode.Data, "----->", "Next", listNode.Next)
 		listNode = listNode.Next
@@ -44,6 +78,17 @@ func main() {
 	AddNewNode("2")
 	AddNewNode("3")
 	AddNewNode("4")
+	AddNewNode("5")
+
+	PrintLinkedList()
+
+	deletedNode := DeleteNode(1)
+	fmt.Println("Deleted Node", deletedNode)
+
+	PrintLinkedList()
+
+	nextDeletedNode := DeleteNode(5)
+	fmt.Println("Deleted Node", nextDeletedNode)
 
 	PrintLinkedList()
 }
